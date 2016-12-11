027a34e72a11103f6c3532c8c95ea253c1bf6a6b
ASaunin <alsaunin@gmail.com>
2016-12-12 02:14:38 +0300

    Work on profile page (not finished)

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -1,4 +1,5 @@
 angular.module('benchmates').controller('tabController', function ($scope) {
+
     $scope.tabs = [{
         name: 'profile',
         title: 'Profile'
@@ -32,5 +33,47 @@ angular.module('benchmates').controller('tabController', function ($scope) {
         }
         return $scope.currentTabName == tabName;
     };
+
+})
+//TODO Provide routing
+.controller('profileController', function ($scope, $location) {
+
+    // $scope.profileId = $location.search().profileId;
+    // getProfile();
+    // function getProfile() {
+    //     $http.get('/profile/'+ $scope.profileId).then(function (response) {
+    //         $scope.profile = response.data;
+    //     });
+    // }
+
+    $scope.profile = {
+        id: 1,
+        first_name: 'Alex',
+        last_name: 'Saunin',
+        sex: 1,
+        email: 'asaunin@mail.ru',
+        phone: '+79110940457',
+        birth_date: "1984-03-23T00:00:00.000Z",
+        reg_date: "2015-01-01T00:00:00.000Z"
+    };
+
+    //ASK How it's better to implement this
+    $scope.formatName = function (profile) {
+        return profile.first_name
+            .concat(" ")
+            .concat(profile.last_name);
+    };
+
+    $scope.formatSex = function (sex) {
+        switch (sex) {
+            case 1:
+                return 'Male';
+            case 2:
+                return 'Male';
+            default:
+                return 'Undefined'
+        }
+    };
+
 });
 
