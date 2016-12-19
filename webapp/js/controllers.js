8bf5ce2fb50a753f00e8a2798d7aa401aacb8de5
ASaunin <alsaunin@gmail.com>
2016-12-19 03:24:04 +0300

    Implement date for yesterday / time for today filter

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -141,6 +141,20 @@ app.filter('startFrom', function () {
     };
 });
 
+app.filter('dateOrTime', function ($filter) {
+    return function (input) {
+        if (input == null) {
+            return "";
+        }
+        var today = new Date();
+        today.setHours(0,0,0,0);
+        if (input.getTime() >= today.getTime()) {
+            return $filter('date') (new Date(input), "HH:mm");
+        }
+        return $filter('date') (new Date(input), "dd.MM.yyyy");
+    };
+});
+
 app.controller('messagesController', ['UserService', 'MessageService', '$http', '$scope',
     function (UserService, MessageService, $http, $scope) {
 
