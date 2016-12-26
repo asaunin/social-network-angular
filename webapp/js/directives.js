c94735d2149d953ba5c44858a3df30e955b7c186
ASaunin <alsaunin@gmail.com>
2016-12-26 10:46:26 +0300

    Refactoring, bugfix, remove # from routing

--- webapp/js/directives.js
+++ webapp/js/directives.js
@@ -6,7 +6,7 @@ app.directive('buttonSendMessage', function () {
         scope: {
             userId: '@'
         },
-        template: '<a href="#/messages/{{userId}}" role="button" class="btn btn-secondary btn-sm"><span class="glyphicon glyphicon-envelope"></span>{{user}} Send message</a>',
+        template: '<a href="#/messages/{{userId}}" role="button" class="btn btn-secondary btn-sm"><span class="glyphicon glyphicon-envelope"></span> Send message</a>',
         replace: true
     };
 });
